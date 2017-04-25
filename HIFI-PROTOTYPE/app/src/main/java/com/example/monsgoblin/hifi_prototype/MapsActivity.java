19a548fd0b88ee8250078f2ba968341b30bf6178
unroofed <p.b@berkeley.edu>
2017-04-25 13:00:18 -0700

    applied some filters

--- HIFI-PROTOTYPE/app/src/main/java/com/example/monsgoblin/hifi_prototype/MapsActivity.java
+++ HIFI-PROTOTYPE/app/src/main/java/com/example/monsgoblin/hifi_prototype/MapsActivity.java
@@ -2,14 +2,36 @@ package com.example.monsgoblin.hifi_prototype;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,6 +54,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private Marker meYou;
     private ArrayList<MarkerWrapper> fakes;
+    private int b1state = 0;
+    private boolean b2state = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +65,140 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Button onlyButton1 = (Button) findViewById(button1);
-        Button onlyButton2 = (Button) findViewById(button2);
+
+
+
+        // Create Object of Dialog class
+        final Dialog login = new Dialog(this);
+        // Set GUI of login screen
+        login.setContentView(R.layout.custom);
+        login.setTitle("Cal@SecureCommunity");
+
+        // Init button of login GUI
+        Button btnLogin = (Button) login.findViewById(R.id.btnLogin);
+        Button btnCancel = (Button) login.findViewById(R.id.btnCancel);
+        final EditText txtUsername = (EditText)login.findViewById(R.id.txtUsername);
+        final EditText txtPassword = (EditText)login.findViewById(R.id.txtPassword);
+
+        // Attached listener for login GUI button
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0)
+                {
+                    // Validate Your login credential here than display message
+                    Toast.makeText(MapsActivity.this,
+                            "Login Sucessfull", Toast.LENGTH_LONG).show();
+
+                    // Redirect to dashboard / home screen.
+                    login.dismiss();
+                }
+                else
+                {
+                    Toast.makeText(MapsActivity.this,
+                            "Please enter Username and Password", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login.dismiss();
+            }
+        });
+
+        // Make dialog box visible.
+        login.show();
+
+        login.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+
+        final Dialog haha = new Dialog(this);
+        // Set GUI of login screen
+        login.setTitle("Cal@SecureCommunity");
+
+
+
+
+        final Button jbutton1 = (Button) findViewById(R.id.button1);
+        final Button jbutton2 = (Button) findViewById(R.id.button2);
+        Button jbutton3 = (Button) findViewById(R.id.button3);
+        Button jbutton4 = (Button) findViewById(R.id.button4);
+        Button jbutton5 = (Button) findViewById(R.id.button5);
+        Button jbutton6 = (Button) findViewById(R.id.button6);
+        Button jbutton7 = (Button) findViewById(R.id.button7);
+
+//vis
+        jbutton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1state = (b1state+1)%3;
+                if (b1state == 1)
+                    jbutton1.setText("INVISIBLE TO ALL");
+                else if (b1state == 2)
+                    jbutton1.setText("VISIBLE TO SAME GENDER");
+                else
+                    jbutton1.setText("VISIBLE TO ALL");
+            }
+        });
+        //gen
+        jbutton2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b2state = !b2state;
+                for (int i = 0; i < fakes.size(); i++) {
+                    if (b2state) {
+                        if (!fakes.get(i).gender.equals("female"))
+                            fakes.get(i).temp.setVisible(false);
+                        jbutton2.setText("SAME GENDER");
+                    }
+                    else {
+                        if (!fakes.get(i).gender.equals("female"))
+                            fakes.get(i).temp.setVisible(true);
+                        jbutton2.setText("ALL GENDERS");
+                    }
+                }
+            }
+        });
+        //dir
+        jbutton3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                haha.show();
+            }
+        });
+        //req
+        jbutton4.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                haha.show();
+            }
+        });
+        //sched
+        jbutton5.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                haha.show();
+            }
+        });
+
+        //rep
+        jbutton6.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                haha.show();
+            }
+        });
+        //watch
+        jbutton7.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                haha.show();
+            }
+        });
+
+
     }
 
 
@@ -76,14 +232,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         meYou.showInfoWindow();
 
         Random mizer = new Random();
-        int rainbows = 100;
+        int rainbows = 1000;
 
         for (int i = 0; i < rainbows; i++) {
             int gender = mizer.nextInt(21);
             float angle = 360*mizer.nextFloat();
             int ready = mizer.nextInt(3);
             Marker tempFake = mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(37.870352 + mizer.nextInt(20 + 1)*.0001*(mizer.nextBoolean() ? -1 : 1), -122.259724 + mizer.nextInt(20 + 1)*.0001*(mizer.nextBoolean() ? -1 : 1)))
+                    .position(new LatLng(37.870352 + mizer.nextInt(100 + 1)*.0001*(mizer.nextBoolean() ? -1 : 1), -122.259724 + mizer.nextInt(100 + 1)*.0001*(mizer.nextBoolean() ? -1 : 1)))
                     .title("TO: Somewhere")
                     .icon(BitmapDescriptorFactory.defaultMarker(angle))
                     .snippet("Hi nice to meet u"));
@@ -99,9 +255,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             tempFake.setRotation(angle);
 
         }
-
-
-
+        /*
+        LoginDialogFragment baby = new LoginDialogFragment();
+        baby.getShowsDialog();
+*/
 
     }
 
@@ -109,19 +266,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         public Marker temp;
         public String gender;
     }
+
 /*
-    public class FireMissilesDialogFragment extends DialogFragment {
+    public static class LoginDialogFragment extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.dialog_fire_missiles)
-                    .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+            builder.setMessage(R.string.username)
+                    .setPositiveButton(R.string.password, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             // FIRE ZE MISSILES!
                         }
                     })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    .setNegativeButton(R.string.password, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             // User cancelled the dialog
                         }
@@ -129,6 +287,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // Create the AlertDialog object and return it
             return builder.create();
         }
+    }
+
+    void showDialog() {
+
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        DialogFragment newFragment = new LoginDialogFragment();
+        String yo = "yo";
+        newFragment.show(getFragmentManager(), yo);
     }
 */
 }
